d5dc23cb35eb117618f76948703c84acb58a5761
livia <liviamajzoubsilva19@gmail.com>
2025-05-15 16:27:45 -0300

    Update exercicio função  -  questão 2  -  Livia.c

--- exercicio função  -  questão 2  -  Livia.c
+++ exercicio função  -  questão 2  -  Livia.c
@@ -6,19 +6,19 @@ int main ()
     int valor_inteiro;
 
     printf("Digite um valor: ");
-    scanf("%f", &valor):
-    
+    scanf("%f", &valor);
+
     valor_inteiro = arredonda(valor);
-    
-    
+
+
     printf("%d\n", valor_inteiro);
-    
-    
+
+
 }
 int arredonda (float valor){
     if(valor > 0)
     return (int) (valor + 0.5);
     else
-    return (int) (valor - 0.5); 
-    
+    return (int) (valor - 0.5);
+
 }
